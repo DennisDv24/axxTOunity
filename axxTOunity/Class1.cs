b91e3c7a922d6b5f0e8b18b4417506a7bae9cc7a
Dennis <jokarboxing@gmail.com>
2019-06-12 23:34:17 +0200

    v001

---
+++ axxTOunity/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace SceneFlow
+{
+    public class changeSceneController
+    {
+		public string sceneToLoad;
+			public void change(){
+				SceneManager.LoadScene(sceneToLoad);
+			}
+
+    }
+}
